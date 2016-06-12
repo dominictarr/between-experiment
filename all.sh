f6e1b7aac9db3e341dd04281ad2f73c46cfd1bc7
Dominic Tarr <dominic.tarr@gmail.com>
2016-06-13 00:01:27 +1200

    pushd writes to stdout on arch!!!

--- all.sh
+++ all.sh
@@ -1,20 +1,17 @@
 
 run () {
 
-  echo run $1
+#  echo run $1 
 
-  rm data/*
+  rm data/* > /dev/null
 
   node random -J $1 -N 1000 --bisecting > data/bisecting
   node random -J $1 -N 1000 > data/between
   node random -J $1 -N 1000 --lex > data/lexiographic
 
-  pushd data
-
-  ../node_modules/.bin/dat-table * --join \
-  | line-graph --width 800 --height 400 --title "average characters per edit, with $1 chance of insert"
-
-  popd
+  dat-table data/* --join \
+  | line-graph --width 800 --height 400 \
+    --title "average characters per edit, with $1 chance of insert"
 
 }
 
@@ -26,4 +23,9 @@ run 0.01 > graphs/J_01.png
 echo compare
 node compare.js | line-graph \
   --width 800 --height 400 --title "characters per edit as a function of J" \
-> graphs/compare.png
+  > graphs/compare.png
+
+
+
+
+
